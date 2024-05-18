199381b7655a07e4808cfc42ed797090001a1637
frebb01 <f.cantiello30@gmail.com>
2024-05-18 17:49:20 +0200

    panel2

--- Assets/Script/Interactor.cs
+++ Assets/Script/Interactor.cs
@@ -1,7 +1,10 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Runtime.CompilerServices;
+using JetBrains.Annotations;
 using Microsoft.Unity.VisualStudio.Editor;
 using TMPro.EditorUtilities;
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class Interactor : MonoBehaviour
@@ -12,6 +15,15 @@ public class Interactor : MonoBehaviour
 
     private Collider[] _colliders = new Collider[2]; // può avere un massimo di 2 oggetti interagibili alla volta
 
+    private Transform[] allTransforms;
+
+    private void Awake()
+    {
+        allTransforms = Resources.FindObjectsOfTypeAll<Transform>(); // lista di tutti i gameObject con una transform
+
+    }
+
+    
     private void Update()
     {
         if (Physics.OverlapSphereNonAlloc(_interactPoint.position, _interactRange, _colliders, _interactableMask) != 0)
@@ -21,19 +33,28 @@ public class Interactor : MonoBehaviour
             string _tagToSearch = interactable.InteractionPrompt;
             if (interactable != null && Input.GetKeyDown(KeyCode.E)) {
                 interactable.Interact(this); // richiamo il metodo Interact
-                //GameObject.FindWithTag(_tagToSearch).SetActive(true);
-                PanelActivation(_tagToSearch);
+                if (_tagToSearch != "Button") {
+                    Debug.Log("ha un panel associato");
+                    PanelActivation(_tagToSearch);
+                    }
             }
            
-
         }
 
     } 
     private void PanelActivation(string _tagToSearch)
-    {
-        GameObject _panel = GameObject.FindWithTag(_tagToSearch);
-        CanvasRenderer canvasRenderer = _panel.GetComponent<CanvasRenderer>();
-        canvasRenderer.SetAlpha(canvasRenderer.GetAlpha() == 1 ? 0 : 1);
+    { 
+        // il problema di questo script è che posso attivare/disattivare il panel 
+        // solo in prossimità dell'oggetto interagibile
+        // vorrei poterlo attivare e disattivare una volta sola
+        // o addirittura disattivarlo dopo un tot di tempo
+        foreach (Transform t in allTransforms)
+        {
+            if (t.CompareTag(_tagToSearch))
+            {
+                t.gameObject.SetActive(!t.gameObject.activeSelf);
+            }
+        }
     }
 }
     
